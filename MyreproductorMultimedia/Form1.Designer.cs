633f5669ca2196d4e94a118dca4bad7fe8b9f3c1
Alumno <Alumno@PC-18>
2022-10-18 12:30:57 -0500

    Agregar archivos de proyecto.

---
+++ MyreproductorMultimedia/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace MyreproductorMultimedia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.mpMediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.lbListaArchivos = new System.Windows.Forms.ListBox();
+            this.Abrir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.mpMediaPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mpMediaPlayer
+            // 
+            this.mpMediaPlayer.Enabled = true;
+            this.mpMediaPlayer.Location = new System.Drawing.Point(45, 27);
+            this.mpMediaPlayer.Name = "mpMediaPlayer";
+            this.mpMediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("mpMediaPlayer.OcxState")));
+            this.mpMediaPlayer.Size = new System.Drawing.Size(314, 353);
+            this.mpMediaPlayer.TabIndex = 0;
+            // 
+            // lbListaArchivos
+            // 
+            this.lbListaArchivos.FormattingEnabled = true;
+            this.lbListaArchivos.Location = new System.Drawing.Point(365, 27);
+            this.lbListaArchivos.Name = "lbListaArchivos";
+            this.lbListaArchivos.Size = new System.Drawing.Size(263, 355);
+            this.lbListaArchivos.TabIndex = 1;
+            this.lbListaArchivos.SelectedIndexChanged += new System.EventHandler(this.lbListaArchivos_SelectedIndexChanged);
+            // 
+            // Abrir
+            // 
+            this.Abrir.Location = new System.Drawing.Point(640, 385);
+            this.Abrir.Name = "Abrir";
+            this.Abrir.Size = new System.Drawing.Size(123, 46);
+            this.Abrir.TabIndex = 2;
+            this.Abrir.Text = "button1";
+            this.Abrir.UseVisualStyleBackColor = true;
+            this.Abrir.Click += new System.EventHandler(this.Abrir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Abrir);
+            this.Controls.Add(this.lbListaArchivos);
+            this.Controls.Add(this.mpMediaPlayer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.mpMediaPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer mpMediaPlayer;
+        private System.Windows.Forms.ListBox lbListaArchivos;
+        private System.Windows.Forms.Button Abrir;
+    }
+}
+
